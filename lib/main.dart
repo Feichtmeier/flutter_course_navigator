c00450d1bd6305652a202bcca08268295527671e
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-05-24 10:19:43 +0200

    add simple nav

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course_navigator/app.dart';
+import 'package:flutter_course_navigator/simple_app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const SimpleApp());
 }
